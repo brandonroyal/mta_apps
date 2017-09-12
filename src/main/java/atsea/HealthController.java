42bdec518d424acfd4690ba86e6cc989e8d322cb
BrandonRoyal <BMRoyal@gmail.com>
2017-09-12 08:32:20 -0400

    added server side home ctrl and healthcheck

--- src/main/java/atsea/HealthController.java
+++ src/main/java/atsea/HealthController.java
@@ -1,4 +1,4 @@
-package hello;
+package atsea;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+public class HealthController {
 
-    @RequestMapping("/greeting")
+    @RequestMapping("/health")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
